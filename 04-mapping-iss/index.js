ccaa203626427ea10f595ec6f6a2f906abe632b3
atharvapatil <atharvaabhaypatil@gmail.com>
2019-11-08 18:00:24 -0500

    iss thing with live data

--- 04-mapping-iss/index.js
+++ 04-mapping-iss/index.js
@@ -1,51 +1,26 @@
 window.addEventListener('load', displayData);
 
-setInterval(displayData, 2000);
+// setInterval(fetchISSLocation, 2000);
+
 
 async function displayData() {
-  fetchISSLocation()
-    .then((data) => {
-      console.log(data);
-      const {
-        latitude,
-        longitude,
-        velocity
-      } = data;
 
-      document.getElementById('lat').textContent = latitude.toFixed(2);
-      document.getElementById('lon').textContent = longitude.toFixed(2);
-      document.getElementById('velocity').textContent = velocity.toFixed(2);
-
-      const map = L.map('issMap').setView([0, 0], 6);
-      const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-      const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-      const tiles = L.tileLayer(tileUrl, {
-        attribution
-      });
-      tiles.addTo(map);
-
-      const issIcon = L.icon({
-        iconUrl: 'issIcon.png',
-        iconSize: [64, 64],
-        iconAnchor: [32, 32]
-      });
-
-      let marker = L.marker([0, 0], {
-        icon: issIcon
-      }).addTo(map);
-
-      map.setView([latitude, longitude], map.getZoom());
-      marker.setLatLng([latitude, longitude]);
-
-    })
-    .catch((error) => {
-      console.error(error);
-    })
-
-}
+const map = L.map('issMap').setView([0, 0], 4);
+    const attribution =
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const tiles = L.tileLayer(tileUrl, { attribution });
+    tiles.addTo(map);
+    // Making a marker with a custom icon
+    const issIcon = L.icon({
+      iconUrl: 'issIcon.png',
+      iconSize: [56, 56],
+      iconAnchor: [28, 28]
+    });
+    let marker = L.marker([0, 0], { icon: issIcon }).addTo(map);
 
 
-// Fetching the ISS data from the API
+      // Fetching the ISS data from the API
 async function fetchISSLocation() {
   const response = await fetch('https://api.wheretheiss.at/v1/satellites/25544');
   const data = await response.json();
@@ -54,10 +29,34 @@ async function fetchISSLocation() {
   const latitude = data.latitude;
   const longitude = data.longitude;
   const velocity = data.velocity;
+  const time = data.timestamp;
 
-  return {
-    latitude,
-    longitude,
-    velocity
-  }
+  // Create a new JavaScript Date object based on the timestamp
+// multiplied by 1000 so that the argument is in milliseconds, not seconds.
+var date = new Date(time*1000);
+// Hours part from the timestamp
+var hours = date.getHours();
+// Minutes part from the timestamp
+var minutes = "0" + date.getMinutes();
+// Seconds part from the timestamp
+var seconds = "0" + date.getSeconds();
+
+// Will display time in 10:30:23 format
+var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+
+  document.getElementById('lat').textContent = latitude.toFixed(2);
+  document.getElementById('lon').textContent = longitude.toFixed(2);
+  document.getElementById('velocity').textContent = velocity.toFixed(2);
+  document.getElementById('time').textContent = formattedTime
+
+  map.setView([latitude, longitude], map.getZoom());
+  marker.setLatLng([latitude, longitude]);
+
+}
+
+fetchISSLocation();
+
+setInterval(fetchISSLocation, 1200);
+
 }
--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Dynamic Web Development - Client Side Experiments
 
-Client side exercises in working with data and fetching information from the Web
+Prototyping experiments with live data
 
-### Live Demos:
-[01. Fetching a local text and image file](https://atharvapatil.github.io/dynamic-web-development/01-fetching-local-text-image/)
-[02. Fetching a local CSV file](https://atharvapatil.github.io/dynamic-web-development/02-fetching-local-csv/)
-[03. Fetching a JSON from a API to display a result](https://atharvapatil.github.io/dynamic-web-development/03-fetching-json/)
+### Live Projects
+- [Get a Random Cocktail Recipe](https://atharvapatil.github.io/dynamic-web-development/thursday-night-in/)
+
+#### Smaller demos:
+- [01. Fetching a local text and image file](https://atharvapatil.github.io/dynamic-web-development/01-fetching-local-text-image/)
+- [02. Fetching a local CSV file](https://atharvapatil.github.io/dynamic-web-development/02-fetching-local-csv/)
+- [03. Fetching a JSON from a API to display a result](https://atharvapatil.github.io/dynamic-web-development/03-fetching-json/)
+- [04. Tracking the live ISS location](https://atharvapatil.github.io/dynamic-web-development/04-mapping-iss/)
